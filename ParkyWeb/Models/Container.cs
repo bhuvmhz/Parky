26b54a1bbee86b198bb6f79713175c1a70be175d
Bhuwan Maharjan <bhuv.mhz@outlook.com>
2020-08-22 11:25:58 +1200

    Added Parky MVC web app, models and a generic repository for type T

---
+++ ParkyWeb/Models/Container.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkyWeb.Models
+{
+    public class Container
+    {
+        public int ContainerId { get; set; }
+        [Required]
+        public string ContainerName { get; set; }
+        [Required]
+        public int Top { get; set; }
+        [Required]
+        public int Left { get; set; }
+        [Required]
+        public int Height { get; set; }
+        [Required]
+        public int Width { get; set; }
+        [Required]
+        public string Color { get; set; }
+        [Required]
+        public string Background { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:5001/";
+
+        public static string NationalParkAPIPath = APIBaseUrl + "api/nationalparks/";
+        public static string TrailAPIPath = APIBaseUrl + "api/trails/";
+        public static string ContainerAPIPath = APIBaseUrl + "api/containers/";
+    }
+}
