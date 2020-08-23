0f3d168bd48cd51c92870995d7210b612ba36752
Bhuwan Maharjan <bhuv.mhz@outlook.com>
2020-08-24 04:26:02 +1200

    auto login cookie authentication and access denied page

--- ParkyWeb/Controllers/HomeController.cs
+++ ParkyWeb/Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -39,10 +42,18 @@ namespace ParkyWeb.Controllers
         public async Task<IActionResult> Login(User obj)
         {
             User authenticatedUser = await _accRepo.LoginAsync(SD.AccountAPIPath + "authenticate/", obj);
-            if (authenticatedUser == null)
+            if (authenticatedUser.Token == null)
             {
                 return View();
             }
+
+            //auto login cookie authentication
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, authenticatedUser.Username));
+            identity.AddClaim(new Claim(ClaimTypes.Role, authenticatedUser.Role));
+            var principal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
             HttpContext.Session.SetString("JWToken", authenticatedUser.Token);
             return RedirectToAction("Index");
         }
@@ -65,10 +76,16 @@ namespace ParkyWeb.Controllers
             return RedirectToAction("Login");
         }
 
-        public IActionResult Logout()
+        public async Task<IActionResult> Logout()
         {
+            await HttpContext.SignOutAsync();
             HttpContext.Session.SetString("JWToken", "");
             return RedirectToAction("Index");
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
